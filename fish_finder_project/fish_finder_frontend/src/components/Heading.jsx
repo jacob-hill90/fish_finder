5118bb1477bbb4a1f7a6f6039d7cf9da250209b9
Jacob Hill <hilljake93@gmail.com>
2022-08-23 15:06:55 -0500

    added some styling

--- fish_finder_project/fish_finder_frontend/src/components/Heading.jsx
+++ fish_finder_project/fish_finder_frontend/src/components/Heading.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 function Heading() {
   return (
     <div className="d-flex justify-content-center align-items-center h-75">
-      <p className="fw-bold text-center" id="heading">Welcome APP</p>
     </div>
   );
 }
