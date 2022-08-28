48dcdd53752a7b4b06683fd91315a2b3657df98e
Jacob Hill <hilljake93@gmail.com>
2022-08-28 12:05:18 -0500

    weather page updates

--- fish_finder_project/fish_finder_frontend/src/pages/WeatherPage.jsx
+++ fish_finder_project/fish_finder_frontend/src/pages/WeatherPage.jsx
@@ -1,11 +1,12 @@
 import WeatherTop from "../components/WeatherTop"
 import WeatherBottom from "../components/WeatherBottom"
 
-function WeatherPage({fullWeather, bigWeatherIcon, city, temp, description, high, low} ){
+function WeatherPage({fullWeather, bigWeatherIcon, city, temp, description, high, low, sunrise, sunset, wind} ){
     console.log(fullWeather)
     return(
-        <div>
-            <WeatherTop fullWeather={fullWeather} bigWeatherIcon={bigWeatherIcon} city={city} temp={temp} description={description} />
+        <div className="weather-page">
+            <h1>Weather Report</h1>
+            <WeatherTop fullWeather={fullWeather} bigWeatherIcon={bigWeatherIcon} city={city} temp={temp} description={description} sunrise={sunrise} sunset={sunset} wind={wind}/>
             <WeatherBottom high={high} low={low}/>
         </div>
     )
